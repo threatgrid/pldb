0d3d54555fc846fe7595c07cd9962181b839ebcd
Norman Richards <orb@nostacktrace.com>
2013-06-23 11:07:00 -0500

    release 0.1.3

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject pldb "0.1.2"
+(defproject pldb "0.1.3"
   :description "PLDB - a persistent logic database for core.logic"
   :url "https://github.com/threatgrid/pldb/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.1"]])
+                 [org.clojure/core.logic "0.8.3"]])
