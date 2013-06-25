4a7f90f034ace1a2b5039d82741e02399667c0de
Norman Richards <orb@nostacktrace.com>
2013-06-24 19:17:10 -0500

    add target-path so lein push works right

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/threatgrid/pldb/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :target-path "target/"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.logic "0.8.3"]])
